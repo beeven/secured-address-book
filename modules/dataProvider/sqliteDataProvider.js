256293aec5f5b77ea38e8e02e64a9dab94b78538
beeven <beeven@hotmail.com>
2014-08-13 17:07:04 +0800

    Update SqliteDataProvider
    Unfinished

--- modules/dataProvider/sqliteDataProvider.js
+++ modules/dataProvider/sqliteDataProvider.js
@@ -7,17 +7,17 @@ var sqlite3 = require("sqlite3");
 var Q = require("q");
 
 function SqliteData() {
-    this.database = new sqlite3.Database(require("path").join(__dirname,"..","..","HRMobile.sqlite"));
+    this.database = new sqlite3.Database(require("path").join(__dirname, "..", "..", "HRMobile.sqlite"));
 
 }
 
-SqliteData.prototype.getDepartments = function(){
+SqliteData.prototype.getDepartments = function () {
     var deferred = Q.defer();
     var db = this.database;
-    db.serialize(function(){
-        db.all('select * from Departments',[],function(err,results){
+    db.serialize(function () {
+        db.all('select * from Departments', [], function (err, results) {
 
-            if(err) {
+            if (err) {
                 deferred.reject(err);
                 return;
             }
@@ -27,17 +27,65 @@ SqliteData.prototype.getDepartments = function(){
     return deferred.promise;
 };
 
-SqliteData.prototype.getDepartmentById = function(deptId) {
+SqliteData.prototype.getDepartmentById = function (deptId) {
     var deferred = Q.defer();
     var db = this.database;
-    db.serialize(function(){
-        db.all('select * from Departments where dept_id = ?',[deptId],function(err,results){
-            if(err) {
+    db.serialize(function () {
+        db.all('select * from Departments where dept_id = ?', [deptId], function (err, results) {
+            if (err) {
                 deferred.reject(err);
                 return;
             }
             deferred.resolve(results);
         })
+    });
+    return deferred.promise;
+};
+
+SqliteData.prototype.getDepartmentTree = function () {
+    var deferred = Q.defer();
+    var db = this.database;
+    db.serialize(function () {
+        var step = 3;
+        var findChildren = function (deptObj) {
+            var d = Q.defer();
+            var ret = [];
+            db.each("select * from Departments where dept_id like ? and length(dept_id) = ?",
+                [deptObj.deptId + '%', deptObj.dpetId.length + step],
+                function (err, row) {
+                    ret.push({
+                        name: row['name'],
+                        fullname: row['fullname'],
+                        id: row['dept_id']
+                    });
+
+                },
+                function (err) {
+                    if (err){
+                        d.reject(err);
+                        return;
+                    }
+                    if(ret.length > 0) {
+                        Q.all(ret.map(function (dInfo) {
+                            return findChildren(dInfo);
+                        })).then(function (results) {
+                            deptObj.children = results;
+                            d.resolve(deptObj);
+                        }).fail(function(err){
+                            console.error(err,deptObj);
+                        })
+                    }
+                    else {
+                        d.resolve(deptObj);
+                    }
+
+                }
+            );
+            return d.promise;
+        };
+        db.get("select * from Departments where dept_id = '051'",function(err,result){
+            return findChildren(result);
+        });
     });
     return deferred.promise;
 };
